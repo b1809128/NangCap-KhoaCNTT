bde5f7a9830f43563ac174858f2007e733aaf676
b1809128 <nghoquochuy0902@gmail.com>
2022-11-25 09:04:03 +0700

    update

--- thi-dua-khen-thuong/createform.php
+++ thi-dua-khen-thuong/createform.php
@@ -42,6 +42,13 @@ session_start(); ?>
     ?>
     <div class="container">
         <?php require("../navbar/navbar.php"); ?>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Library</a></li>
+                <li class="breadcrumb-item" aria-current="page">Data</li>
+            </ol>
+        </nav>
         <div class="row">
             <form action="" method="post">
                 <h5 style="color: #0d6efd;">TẠO FORM BẦU CHỌN</h5>
